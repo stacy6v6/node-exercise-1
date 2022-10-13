83358e831b9a6fa9933653c5ef87e2a550e0792f
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2022-10-13 15:12:47 +0800

    Modified file finder with early return added

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 const fs = require('fs')
 
-if (process.argv.slice(2).length < 2 || process.argv.slice(2).length > 3) {
-  console.log('Invalid no. of arguments')
-} else {
-  fs.readFile(process.argv[2].toString(), 'utf-8', (err, data) => {
-    let finalData = ''
-    for (let i = 0; i < data.split('\n').length; i += 1) {
-      const linecount = i + 1
-      let lineData
+const [inputFile, outputFile, flag] = process.argv.slice(2)
 
-      for (let x = 0; x < data.split('\n').length; x += 1) {
-        if (data[x] === '\n') {
-          lineData = data.split('\n')
-          finalData += `${linecount}: ${lineData[i]}\n`
-        }
-      }
-    }
-    if (err) {
-      console.log('file cannot be found.')
-    } else if (data) {
-      if (fs.existsSync(process.argv[3]) === true) {
-        if (process.argv[4] === undefined || process.argv[4] === '-n') {
-          console.log('File already exists and will not be overwritten')
-        } else if (process.argv[4] === '-y') {
-          fs.writeFile(process.argv[3], finalData, (error) => {
-            if (error) {
-              console.log(error)
-            }
-          })
-        } else {
-          console.log('Invalid argument. Please input -y or -n only for the third argument.')
-        }
-      } else {
-        fs.writeFile(process.argv[3], finalData, (error) => {
-          if (error) {
-            console.log(error)
-          }
-        })
-      }
-    }
-  })
+if (inputFile === undefined || outputFile === undefined) {
+  console.log('Invalid no. of arguments')
+  process.exit(1)
 }
+
+fs.readFile(inputFile.toString(), 'utf-8', (err, data) => {
+  if (err) {
+    console.log('file cannot be found.')
+    process.exit(1)
+  }
+
+  let finalData = ''
+  const dataStr = data.split('\n')
+  for (let i = 0; i < dataStr.length; i += 1) {
+    finalData += `${i + 1}: ${dataStr[i]}\n`
+  }
+
+  if (fs.existsSync(outputFile) === true) {
+    if (flag !== '-y' && flag !== '-n' && flag !== undefined) {
+      console.log('Invalid argument. Please input -y or -n only for the third argument.')
+      process.exit(1)
+    }
+    if (flag === undefined || flag === '-n') {
+      console.log('File already exists and will not be overwritten')
+      process.exit(1)
+    }
+    if (flag === '-y') {
+      fs.writeFile(outputFile, finalData, (error) => {
+        if (error) {
+          console.log(error)
+        }
+      })
+    }
+  } else {
+    fs.writeFile(outputFile, finalData, (error) => {
+      if (error) {
+        console.log(error)
+      }
+    })
+  }
+})
